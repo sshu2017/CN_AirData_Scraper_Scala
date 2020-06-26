0132be6b3de0dce5e5dc8ae0bd629b4b855a029f
shushi20002003@gmail.com-at-644876091492 <shushi20002003@gmail.com>
2020-06-26 14:32:02 -0700

    Initial Commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "cnAirScraper"
+
+version := "0.1"
+
+scalaVersion := "2.13.2"
+
+libraryDependencies += "org.jsoup" % "jsoup" % "1.8.3"
---
+++ src/main/scala/CNAirScraper.scala
@@ -0,0 +1,49 @@
+import org.jsoup.Jsoup
+import scala.jdk.CollectionConverters._
+import java.io._
+
+object CNAirScraper extends App {
+
+  val html = Jsoup.connect("http://pm25.in/").get()
+  val cities = html.select(".unstyled > div > li > a").asScala
+
+  var cnt: Int = 1
+  for (city <- cities) {
+    if (cnt <= 3) {
+      saveOneCityTable(
+        getOneCityData("http://pm25.in" + city.attr("href"))
+      )
+      cnt = cnt + 1
+      print("cnt = " + cnt + "\n")
+    }
+  }
+
+  def getOneCityData(inputURL: String): String = {
+    println(inputURL)
+    val cityDoc = Jsoup.connect(inputURL).get()
+    val cityTable = cityDoc.select("#detail-data")
+    //    be nice to the server
+    Thread.sleep(1500)
+    print("Sleeping for 1.5 seconds ...\n")
+    cityTable.toString
+  }
+
+  def saveOneCityTable(inputTable: String) = {
+    val pw = new PrintWriter(new FileWriter(("data.txt"), true))
+
+    val oneCityTable = Jsoup.parse(inputTable)
+
+    for (ln <- oneCityTable.select("th").asScala) (pw.write(ln.text + "|"))
+
+    for (ln <- oneCityTable.select("tr").asScala) {
+      for (e <- ln.select("td").asScala) {
+        pw.write(e.text + "|")
+      }
+      pw.write("\n")
+    }
+
+    pw.close()
+  }
+}
+
+
